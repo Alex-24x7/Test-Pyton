8d7175ed2e8b203527772ead11b39fe822cd9b33
AlexWork <>
2022-10-07 15:43:14 +0300

    add 3

--- new.py
+++ new.py
@@ -33,3 +33,33 @@ clear()
 # a = int(input())
 # a = a*45 + (a//2)*5 + ((a+1)//2-1)*15
 # print(a//60 + 9, a%60)
+
+# Задача 5
+# a = int(input())
+# b = int(input())
+# n = int(input())
+# rub = int(a*n)
+# cop = int(b*n)
+# while cop > 99:
+#     rub += 1
+#     cop -= 100
+# print(rub, cop)
+
+# Задача 6
+# h1 = int(input())*3600
+# m1 = int(input())*60
+# s1 = int(input())
+# h2 = int(input())*3600
+# m2 = int(input())*60
+# s2 = int(input())
+# print((h2 + m2 + s2)-(h1 + m1 + s1))
+
+# Задача 7
+# kmS = int(input())
+# km = int(input())
+
+# print(-1 * km // kmS * -1)
+
+# Задача 8
+# year = int(input())
+# print('YES' if ((year%4)==0 and (year%100)!=0) or (year%400)==0 else 'NO')
